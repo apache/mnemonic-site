4e67e9bdad1fd9bf9bda0c601524081d840ad4df
Yanhui Zhao <yzhao@apache.org>
2022-01-09 05:40:05 +0000

    MNEMONIC-701: Setup backend web framework skeleton with current request interfaces

--- fastapi/server.py
+++ fastapi/server.py
@@ -6,3 +6,27 @@ app = FastAPI()
 @app.get("/")
 def mnemonic_home():
     return {"Apache Mnemonic Home"}
+
+@app.get("/docs")
+def mnemonic_docs():
+    return {"Mnemonic documentation home page"}
+
+@app.get("/downloads")
+def mnemonic_downloads():
+    return {"Mnemonic downloads home page"}
+
+@app.get("/talks")
+def mnemonic_talks():
+    return {"Mnemonic talks home page"}
+
+@app.get("/news")
+def mnemonic_news():
+    return {"Mnemonic news home page"}
+
+@app.get("/develop")
+def mnemonic_develop():
+    return {"Mnemonic development home page"}
+
+@app.get("/help")
+def mnemonic_help():
+    return {"Mnemonic help home page"}
