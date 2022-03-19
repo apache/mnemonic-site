35aa332f79cc483ceb9d39bbe0deef5ff868c0a9
Yanhui Zhao <wildyz.yky@gmail.com>
2022-03-18 21:04:20 -0700

    MNEMONIC-717: Create webpage templates that can be shared and used for each section home pages

--- mnemonic-fastapi/app/main.py
+++ mnemonic-fastapi/app/main.py
@@ -4,7 +4,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-from .library.helpers import *
+#from .library.helpers import *
 
 app = FastAPI()
 
